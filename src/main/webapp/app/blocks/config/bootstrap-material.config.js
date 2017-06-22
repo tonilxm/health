b93030e3d4bf4c7d4a4de8a1c2198f4dc7afa704
Toni Liem <toni.xm@gmail.com>
2017-06-22 21:28:52 +0700

    Add Deployment Setting to Heroku

---
+++ src/main/webapp/app/blocks/config/bootstrap-material.config.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('21PointsApp')
+        .config(bootstrapMaterialDesignConfig);
+
+    bootstrapMaterialDesignConfig.$inject = [];
+
+    function bootstrapMaterialDesignConfig() {
+        $.material.init();
+
+    }
+})();
